aed038b1cd2b66ca40b0118744b9391d1b859491
Dwin Gharibi <dwin.gharibi@email.kntu.ac.ir>
2025-03-26 12:43:19 +0330

    feat(docker-compose.yml): updating docker-compose file to serve dart sdk using python docker handler

--- solution/solution.dart
+++ solution/solution.dart
@@ -1,11 +1,7 @@
 import 'dart:io';
 
 void main() {
-    String? input1 = stdin.readLineSync();
-    String? input2 = stdin.readLineSync();
-
-    int num1 = int.parse(input1);
-    int num2 = int.parse(input2);
-    int sum = num1 + num2;
-    print("$sum");
+  int a = int.parse(stdin.readLineSync()!);
+  int b = int.parse(stdin.readLineSync()!);
+  print(a + b);
 }
--- test.py
+++ test.py
@@ -8,17 +8,18 @@ class TestDartPrograms(unittest.TestCase):
     def setUpClass(cls):
         DockerHandler.start_container()
 
-    @classmethod
-    def tearDownClass(cls):
-        DockerHandler.stop_container()
+    # @classmethod
+    # def tearDownClass(cls):
+    #     DockerHandler.stop_container()
+
 
     def test_1(self):
         dart_file = "solution.dart"
         input_file = "input4.txt"
         output_file = "output4.txt"
 
-        DartRunner.compile_asm(dart_file)
-        output = DartRunner.run_asm(dart_file, input_file)
+        DartRunner.compile_dart(dart_file)
+        output = DartRunner.run_dart(dart_file, input_file)
 
         output_path = os.path.join(DartRunner.TEST_CASES_FOLDER, "out", output_file)
         with open(output_path, "r") as f:
